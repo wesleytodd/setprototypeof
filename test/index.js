6d8bbc483ef1018e4a73e1cb5a58912c9ab5ac00
phapdinh <centralparkloop@yahoo.com>
2018-11-06 19:48:08 -0500

    add unit test

---
+++ test/index.js
@@ -0,0 +1,28 @@
+'use strict'
+var assert = require('assert')
+var setPrototypeOf = require('..')
+
+function protoTest (obj, proto) {
+  if (Object.getPrototypeOf) {
+    assert.strictEqual(Object.getPrototypeOf(obj), proto)
+  } else if ({__proto__:[]} instanceof Array) {
+    assert.strictEqual(obj.__proto__, proto)
+  } else {
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        assert.strictEqual(obj[prop], proto[prop])
+      }
+    }
+  }
+}
+
+describe('setProtoOf(obj, proto)', function() {
+	it('should merge objects', function(){
+		var obj = { a: 1, b: 2 }
+		var proto = { c: 3, d: 4 }
+		var mergeObj = setPrototypeOf(obj, proto);
+    protoTest(obj, proto)
+    protoTest(mergeObj, proto)
+    assert.strictEqual(mergeObj, obj)
+	})
+})
