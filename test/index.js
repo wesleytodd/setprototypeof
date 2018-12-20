f3c0a87150ff369a2a85a5a3350bea3aabaad020
Wes Todd <wes@wesleytodd.com>
2018-12-19 16:11:53 -0800

    added standard linting

--- test/index.js
+++ test/index.js
@@ -1,11 +1,13 @@
 'use strict'
+/* eslint-env mocha */
+/* eslint no-proto: 0 */
 var assert = require('assert')
 var setPrototypeOf = require('..')
 
 function protoTest (obj, proto) {
   if (Object.getPrototypeOf) {
     assert.strictEqual(Object.getPrototypeOf(obj), proto)
-  } else if ({__proto__:[]} instanceof Array) {
+  } else if ({ __proto__: [] } instanceof Array) {
     assert.strictEqual(obj.__proto__, proto)
   } else {
     for (var prop in obj) {
@@ -16,13 +18,13 @@ function protoTest (obj, proto) {
   }
 }
 
-describe('setProtoOf(obj, proto)', function() {
-	it('should merge objects', function(){
-		var obj = { a: 1, b: 2 }
-		var proto = { c: 3, d: 4 }
-		var mergeObj = setPrototypeOf(obj, proto);
+describe('setProtoOf(obj, proto)', function () {
+  it('should merge objects', function () {
+    var obj = { a: 1, b: 2 }
+    var proto = { c: 3, d: 4 }
+    var mergeObj = setPrototypeOf(obj, proto)
     protoTest(obj, proto)
     protoTest(mergeObj, proto)
     assert.strictEqual(mergeObj, obj)
-	})
+  })
 })
