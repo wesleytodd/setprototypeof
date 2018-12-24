f796a658bf6753dde3c9a6aaab079ba2d825c787
Wes Todd <wtodd@netflix.com>
2018-12-24 12:32:42 -0800

    simplified test and fixed mixin test

--- test/index.js
+++ test/index.js
@@ -4,27 +4,21 @@
 var assert = require('assert')
 var setPrototypeOf = require('..')
 
-function protoTest (obj, proto) {
-  if (Object.getPrototypeOf) {
-    assert.strictEqual(Object.getPrototypeOf(obj), proto)
-  } else if ({ __proto__: [] } instanceof Array) {
-    assert.strictEqual(obj.__proto__, proto)
-  } else {
-    for (var prop in obj) {
-      if (obj.hasOwnProperty(prop)) {
-        assert.strictEqual(obj[prop], proto[prop])
-      }
-    }
-  }
-}
-
 describe('setProtoOf(obj, proto)', function () {
   it('should merge objects', function () {
     var obj = { a: 1, b: 2 }
-    var proto = { c: 3, d: 4 }
+    var proto = { b: 3, c: 4 }
     var mergeObj = setPrototypeOf(obj, proto)
-    protoTest(obj, proto)
-    protoTest(mergeObj, proto)
+
+    if (Object.getPrototypeOf) {
+      assert.strictEqual(Object.getPrototypeOf(obj), proto)
+    } else if ({ __proto__: [] } instanceof Array) {
+      assert.strictEqual(obj.__proto__, proto)
+    } else {
+      assert.strictEqual(obj.a, 1)
+      assert.strictEqual(obj.b, 2)
+      assert.strictEqual(obj.c, 4)
+    }
     assert.strictEqual(mergeObj, obj)
   })
 })
